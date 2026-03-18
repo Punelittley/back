7aade64f672cccb7880ed009afd04491e6d10e5c
Punelittley <bapenick445@gmail.com>
2026-03-19 01:34:55 +0500

    first st

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,16 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: true,
+    allowedHosts: ['back-xlm1.onrender.com'], 
+    host: '0.0.0.0',
+    port: 10000,
     proxy: {
       '/api': {
-        target: 'http://localhost:4000',
+        target: 'http://localhost:4000', 
         changeOrigin: true,
-      },
-    },
-  },
+        secure: false,
+      }
+    }
+  }
 })
